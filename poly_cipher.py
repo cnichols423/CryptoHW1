25d4842a386e6ccd868834a5de9f04e61fdc824f
Cohen Nichols <101533757+cnichols423@users.noreply.github.com>
2022-09-11 21:17:42 -0400

    Update poly_cipher.py

--- poly_cipher.py
+++ poly_cipher.py
@@ -2,4 +2,45 @@
 # poly_cipher.py
 # hw 1
 
-print("enter a message you would like to see encrypted: ")
+############################################################################
+## algorithm to take txt and encrpyt it
+def text_to_cipher(userInput, encryptKey):
+    key = list(encryptKey)
+    if len(userInput) == len(key):
+        return(key)
+    else:
+        for i in range(len(userInput) - len(key)):
+            key.append(key[i % len(key)])
+    key = "".join(key)
+    cipherText =[]
+    for i in range(len(userInput)):
+        a = (ord(userInput[i]) + ord(key[i]) + 26) % 26
+        a += ord('A')
+        cipherText.append(chr(a))
+    return("".join(cipherText))
+
+##############################################################################
+## take cipher and turn to text
+def cipher_to_text(cipherText, key):
+    originalText = []
+    for i in range(len(cipherText)):
+        a = (ord(cipherText[i]) + ord(key[i]) + 26) % 26
+        a += ord('A')
+        cipherText.append(chr(a))
+    return("".join(originalText))
+
+##############################################################################
+## main
+   
+def main():
+    print("\n")
+    print("showing encyrption")
+if __name__ == "__main__":
+     main()
+
+userInput = input("enter a message you would like to see encrypted: ")
+encryptKey = input("enter the key for encryption: ")
+
+
+print("\n")
+print("encrypted text: ", text_to_cipher(userInput,encryptKey))
